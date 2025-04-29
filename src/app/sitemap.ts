797b0e0a3b4ccebeb5a958a48d719e913bf71aa9
Halil Tunc <hitunc@outlook.com>
2025-04-29 12:42:07 +0300

    chore: add sitemap.xml and robots.txt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const domain = "https://www.datavisly.com";
+  const pageUrls = [{ url: "" }, { url: "/login" }, { url: "/sign-up" }];
+
+  return pageUrls.map((page) => {
+    return {
+      url: domain + page.url,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: page.url === "" ? 1 : 0.8,
+    };
+  });
+}
