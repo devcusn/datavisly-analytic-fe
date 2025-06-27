7567e123eefadd95435f8c50c3abd946396fcdaf
Halil Tunc <hitunc@outlook.com>
2025-06-27 10:25:05 +0300

    chore(env): Add backend api url to axios interceptor

--- src/services/_base/index.ts
+++ src/services/_base/index.ts
@@ -1,4 +1,5 @@
 import AxiosInterceptor from "./interceptor";
 
-export const request = new AxiosInterceptor("http://localhost:4000/api/v1")
-  .instance;
+export const request = new AxiosInterceptor(
+  process.env.NEXT_PUBLIC_BACKEND_API_URL as string
+).instance;
